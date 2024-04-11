fa1a774807a59e4e37323cd9423a28445acdc093
sujan <sujanunni1718@gmail.com>
2024-04-11 09:26:58 +0530

    pinecone

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -19,7 +19,8 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                     placeholder="Enter your question...." 
                     className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
                    />
-                   <Button aria-label="send message">
+                   <Button className='absolute bottom-1.5 right-[8px]'
+                    aria-label="send message">
                       <Send className="h-4 w-4" />
                    </Button>
                 </div>
