fb38f227e7bd066ee092f92d58674286bdcf56d0
jwalish <jwalishj@gmail.com>
2024-04-17 11:02:38 +0530

    edited core.ts

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,12 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+
+import { PDFLoader} from 'langchain/document_loaders/fs/pdf'
+import {OpenAIEmbeddings} from "langchain/embeddings/openai"
+import {PineconeStore} from "@langchain/pinecone"
+import { pinecone } from "@/lib/pinecone";
+
 
 const f = createUploadthing();
 
@@ -27,6 +32,49 @@ export const ourFileRouter = {
         },
       })
 
+      try {
+        const response = await fetch (`https://utfs.io/f/${file.key}`)
+        const blob = await response.blob()
+
+        const loader = new PDFLoader(blob)
+
+        const pageLevelDocs = await loader.load()
+        const pagesAmt = pageLevelDocs.length
+
+       
+        const pineconeIndex = pinecone.Index('docchat')
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY
+        })
+
+        await PineconeStore.fromDocuments(pageLevelDocs,embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+
+        }
+      )
+
+      await db.file.update({
+        data: {
+          uploadStatus: "SUCCESS"
+        },
+        where:{
+          id:createdFile.id
+        }
+      })
+
+      } catch (err) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED"
+          },
+          where:{
+            id:createdFile.id
+          }
+        })
+        
+      }
 
 
     }),
