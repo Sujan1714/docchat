244aff5675b6d73cd4d2adf83c5975349c09850e
jwalish <jwalishj@gmail.com>
2024-04-27 09:37:03 +0530

    updated BillingForm

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,15 @@ import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { ArrowRight } from "lucide-react"
 import { buttonVariants } from "./ui/button"
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { LoginLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import UserAccountNav from "./UserAccountNav"
+
 
 const Navbar = () => {
+
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -16,7 +22,7 @@ const Navbar = () => {
                     {/* todo: add mobile navbar */}
 
                     <div className='hidden items-center space-x-4 sm:flex'>
-                        <>
+                        {!user ? <>
                             <Link href='/pricing'
                                 className={buttonVariants({
                                     variant: 'ghost',
@@ -37,7 +43,19 @@ const Navbar = () => {
                                 Get started
                                 <ArrowRight className='ml-1.5 h-5 w-5' />
                             </RegisterLink>
-                        </>
+                        </> : <>
+                        <Link href='/dashboard'
+                                className={buttonVariants({
+                                    variant: 'ghost',
+                                    size: 'sm',
+                                })}>
+                                    Dashboard
+                        </Link>
+                        
+                        <UserAccountNav
+                        
+                        />
+                        </>}
                     </div>
 
                 </div>
